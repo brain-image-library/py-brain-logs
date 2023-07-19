c155082618d5aa8da13ab2332ba9cc964eaa1e53
Ivan Cao-Berg <icaoberg@psc.edu>
2023-07-19 01:39:20 -0400

    1. Updated the list of methods to use double quotes instead of single quotes. (changed: methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'TRACE', 'PATCH'] to methods = ["GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "TRACE", "PATCH"]) 2. Changed the file path to use double quotes instead of single quotes. (changed: with open('data/access.log', 'r') as file: to with open("data/access.log", "r") as file:) 3. Fixed an indentation issue for creating the split_request list. (changed: split_request = request_line.split(' ') to split_request = request_line.split(" ")) 4. Fixed an issue where the 'url' variable was assigned incorrect value. (changed: url = split_request[1] if split_request[0] in methods else split_request[0]) 5. Fixed an issue where the 'is_bot' value was not correctly assigned. (changed: "is_bot": 'bot' in user_agent.lower() to "is_bot": "bot" in user_agent.lower())

--- read_log.py
+++ read_log.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 import pandas as pd
 
-methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'TRACE', 'PATCH']
+methods = ["GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "TRACE", "PATCH"]
 pattern = r'^([\d.]+) - - \[([^]]+)\] "([^"]*)" (\d+) (\d+) "([^"]*)" "([^"]*)" "-"$'
 
-with open('data/access.log', 'r') as file:
+with open("data/access.log", "r") as file:
     data = []
     lines = file.readlines()
 
@@ -21,21 +21,21 @@ with open('data/access.log', 'r') as file:
             referrer = matches.group(6)
             user_agent = matches.group(7)
 
-            split_request = request_line.split(' ')
+            split_request = request_line.split(" ")
 
             method = split_request[0] if split_request[0] in methods else ""
             url = split_request[1] if split_request[0] in methods else split_request[0]
 
             dataset = {
                 "ip": ip_address,
-                "date": datetime.strptime(timestamp, '%d/%b/%Y:%H:%M:%S %z'),
+                "date": datetime.strptime(timestamp, "%d/%b/%Y:%H:%M:%S %z"),
                 "method": method,
                 "url": url,
                 "status_code": status_code,
                 "size": size,
                 "referrer": referrer,
                 "user_agent": user_agent,
-                "is_bot": 'bot' in user_agent.lower()
+                "is_bot": "bot" in user_agent.lower(),
             }
 
             data.append(dataset)
